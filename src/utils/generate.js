752f3c21c64348603f9bdd33b86c6d121b4e9404
unknown <n.ustinov93@gmail.com>
2024-03-07 16:52:46 +0200

    Update generate.js

--- src/utils/generate.js
+++ src/utils/generate.js
@@ -37,8 +37,8 @@ export const generate = async (file, locationName = "") => {
     error: false,
     data: {
       classes: generateClasses(resultArray),
-      courses: generateCourses(resultArray),
-      location: generateLocation(resultArray),
+      courses: generateCourses(resultArray, locationName),
+      location: generateLocation(resultArray, locationName),
       rosters: generateRosters(resultArray),
       staff: generateStaff(resultArray),
       students: generateStudents(resultArray),
@@ -58,8 +58,8 @@ const generateClasses = (array) => {
   ];
 
   const data = array.map((raw) => {
-    const class_id = getClassId(raw["班級"]);
-    return [class_id, raw["班級"], " ", " ", " ", " ", "SAMPLE-LOC-1"];
+    const { class_id, course_id } = getClassId(raw["班級"]);
+    return [class_id, raw["班級"], course_id, " ", " ", " ", "SAMPLE-LOC-1"];
   });
 
   return {
@@ -68,10 +68,37 @@ const generateClasses = (array) => {
   };
 };
 
-const generateCourses = (array) => {
+const generateCourses = (array, location_name) => {
   const headers = ["course_id", "course_number", "course_name", "location_id"];
 
-  const data = [[" ", " ", " ", " "]];
+  const coursesIds = [
+    "Junior-1",
+    "Junior-2",
+    "Junior-3",
+    "Junior-4",
+    "Junior-5",
+    "Junior-6",
+    "Junior-7",
+    "Junior-8",
+    "Junior-11",
+    "Junior-12",
+    "Junior-13",
+    "Junior-14",
+    "Junior-15",
+    "Junior-16",
+    "Junior-17",
+    "Junior-21",
+    "Junior-22",
+    "Junior-23",
+    "Junior-24",
+    "Junior-25",
+    "Junior-26",
+    "Junior-27",
+  ];
+
+  const data = coursesIds.map((courseId) => {
+    return [courseId, " ", " ", location_name];
+  });
 
   return {
     headers,
@@ -79,9 +106,9 @@ const generateCourses = (array) => {
   };
 };
 
-const generateLocation = (array) => {
+const generateLocation = (array, location_name) => {
   const headers = ["location_id", "location_name"];
-  const data = [["SAMPLE-LOC-1", "ctjhs"]];
+  const data = [["SAMPLE-LOC-1", location_name]];
 
   return { headers, data };
 };
@@ -90,7 +117,7 @@ const generateRosters = (array) => {
   const headers = ["roster_id", "class_id", "student_id"];
   const data = array.map((raw) => {
     const roster_id = "st" + raw["學號"];
-    const class_id = getClassId(raw["班級"]);
+    const { class_id } = getClassId(raw["班級"]);
     const student_id = "student" + raw["學號"];
     return [roster_id, class_id, student_id];
   });
@@ -193,73 +220,142 @@ const xlsxToArray = (file) => {
 const getClassId = (classNumber) => {
   switch (classNumber) {
     case "七年一班":
-      return 101;
+      return {
+        class_id: 101,
+        course_id: "Junior-1",
+      };
       break;
     case "七年二班":
-      return 102;
+      return {
+        class_id: 102,
+        course_id: "Junior-2",
+      };
       break;
     case "七年三班":
-      return 103;
+      return {
+        class_id: 103,
+        course_id: "Junior-3",
+      };
       break;
     case "七年四班":
-      return 104;
+      return {
+        class_id: 104,
+        course_id: "Junior-4",
+      };
       break;
     case "七年五班":
-      return 105;
+      return {
+        class_id: 105,
+        course_id: "Junior-5",
+      };
       break;
     case "七年六班":
-      return 106;
+      return {
+        class_id: 106,
+        course_id: "Junior-6",
+      };
       break;
     case "七年七班":
-      return 107;
+      return {
+        class_id: 107,
+        course_id: "Junior-7",
+      };
       break;
     case "七年八班":
-      return 108;
+      return {
+        class_id: 108,
+        course_id: "Junior-8",
+      };
       break;
     case "八年一班":
-      return 201;
+      return {
+        class_id: 201,
+        course_id: "Junior-11",
+      };
       break;
     case "八年二班":
-      return 202;
+      return {
+        class_id: 202,
+        course_id: "Junior-12",
+      };
       break;
     case "八年三班":
-      return 203;
+      return {
+        class_id: 203,
+        course_id: "Junior-13",
+      };
       break;
     case "八年四班":
-      return 204;
+      return {
+        class_id: 204,
+        course_id: "Junior-14",
+      };
       break;
     case "八年五班":
-      return 205;
+      return {
+        class_id: 205,
+        course_id: "Junior-15",
+      };
       break;
     case "八年六班":
-      return 206;
+      return {
+        class_id: 206,
+        course_id: "Junior-16",
+      };
       break;
     case "八年七班":
-      return 207;
+      return {
+        class_id: 207,
+        course_id: "Junior-17",
+      };
       break;
     case "九年一班":
-      return 301;
+      return {
+        class_id: 301,
+        course_id: "Junior-21",
+      };
       break;
     case "九年二班":
-      return 302;
+      return {
+        class_id: 302,
+        course_id: "Junior-22",
+      };
       break;
     case "九年三班":
-      return 303;
+      return {
+        class_id: 303,
+        course_id: "Junior-23",
+      };
       break;
     case "九年四班":
-      return 304;
+      return {
+        class_id: 304,
+        course_id: "Junior-24",
+      };
       break;
     case "九年五班":
-      return 305;
+      return {
+        class_id: 305,
+        course_id: "Junior-25",
+      };
       break;
     case "九年六班":
-      return 306;
+      return {
+        class_id: 306,
+        course_id: "Junior-26",
+      };
       break;
     case "九年七班":
-      return 307;
+      return {
+        class_id: 307,
+        course_id: "Junior-27",
+      };
       break;
     default:
-      return 0;
+      return {
+        class_id: 0,
+        course_id: " ",
+      };
       break;
   }
 };
